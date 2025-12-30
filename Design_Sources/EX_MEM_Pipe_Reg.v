90fc88f4ca090c249f8ae8a8231ee580e4820345
N_VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-12-30 14:52:18 +0530

    Update EX_MEM_Pipe_Reg.v

--- Design_Sources/EX_MEM_Pipe_Reg.v
+++ Design_Sources/EX_MEM_Pipe_Reg.v
@@ -42,27 +42,6 @@ module EX_MEM_Pipe_Reg(
     output reg branch_taken_out
     );
     
-    always @(posedge clk) begin
-        if(rst) begin
-            alu_result_out <= 32'd0;
-            rs2_data_out <= 32'd0;
-            rd_out <= 5'd0;
-            reg_write_out <= 1'b0;
-            mem_write_out <= 1'b0;
-            mem_read_out <= 1'b0;
-            mem_to_reg_out <= 1'b0;
-            branch_taken_out <= 1'b0;
-        end
-        else begin
-            alu_result_out <= alu_result_in;
-            rs2_data_out <= rs2_data_in;
-            rd_out <= rd_in;
-            reg_write_out <= reg_write_in;
-            mem_write_out <= mem_write_in;
-            mem_read_out <= mem_read_in;
-            mem_to_reg_out <= mem_to_reg_in;
-            branch_taken_out <= branch_taken_in;
-        end
-    end
-    
+    // Internal implementation omitted in public version.
+   // Complete source is available for technical evaluation.
 endmodule
