1be9e15642c9c8af3f3e0024972c1757a538371e
N_VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-12-30 14:53:03 +0530

    Update IF_ID_Pipeline_Register.v

--- Design_Sources/IF_ID_Pipeline_Register.v
+++ Design_Sources/IF_ID_Pipeline_Register.v
@@ -28,15 +28,7 @@ module IF_ID_Pipeline_Register(
     output reg[31:0] pc_out,
     output reg[31:0] instruction_out
     );
-    always @(posedge clk) begin
-        if(rst) begin
-            pc_out <= 32'd0;
-            instruction_out <= 32'd0;
-        end
-        else begin
-            pc_out <= pc_in;
-            instruction_out <= instruction_in;
-        end
-    end
+    // Internal implementation omitted in public version.
+   // Complete source is available for technical evaluation.
     
 endmodule
